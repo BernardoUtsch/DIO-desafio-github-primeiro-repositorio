f8c72eb09d51780e3b2a6807b0fe3254f3ded721
BernardoUtsch <bernardoutsch@gmail.com>
2022-07-22 12:39:00 -0300

    contador

--- Modulo III - Conhecendo a linguagem JS/assets/js/teste.js
+++ Modulo III - Conhecendo a linguagem JS/assets/js/teste.js
@@ -1,4 +1,4 @@
-function returnEvenValues(array){
+/*function returnEvenValues(array){
     let evenNums = [];
     for (let i = 0; i < array.length; i++)
         if(array[i] %2 ===0) {
@@ -8,4 +8,22 @@ function returnEvenValues(array){
     }
 let array = [56, 1, 2, 3, 4, 5, 6, 5, 5, 8, 9, 65, 5, 6, 5, 5, 6, 1, 32, 32, 11, 65, 165, 65, 65, 18, 498, 495, 16, 549, 84, 951, 65, 1];
 
-returnEvenValues(array);
+returnEvenValues(array);*/
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment(){
+    count++;
+    CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement(){
+    count--;
+    CURRENT_NUMBER.innerHTML = count;
+}
+
+function test(){
+    kdowkdpo;
+}
